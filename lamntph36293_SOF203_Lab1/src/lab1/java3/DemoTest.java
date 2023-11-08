cf0d62e42ce9dc747a3d1ad5a1ef4ea75f7dc134
DESKTOP-UM8PGTH\pc <tuannvph36572@fpt.edu.vn>
2023-11-08 09:05:51 +0700

    Tuấn done ......

---
+++ lamntph36293_SOF203_Lab1/src/lab1/java3/DemoTest.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab1.java3;
+
+/**
+ *
+ * @author pc
+ */
+public class DemoTest {
+    public static void main(String[] args) {
+        System.out.println("Demo");
+    }
+}
